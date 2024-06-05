710249d8b89ab22c96d52f4446fd7f36abdfaf7c
Ritu0097 <rituchaudhary0097@gmail.com>
2024-06-05 14:45:00 +0530

    remove

--- main.js
+++ main.js
@@ -6,9 +6,29 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+function removeWordFromFile(filename, word) {
+  fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Error reading file: ${err}`);
+      return;
+    }
+
+    const modifiedContent = data.replace(new RegExp(word, 'gi'), '');
+
+    fs.writeFile(filename, modifiedContent, 'utf8', (err) => {
+      if (err) {
+        console.error(`Error writing to file: ${err}`);
+        return;
+      }
+
+      console.log(`All occurrences of "${word}" have been removed from ${filename}.`);
+    });
+  });
+}
+
 rl.question('Enter the filename: ', (filename) => {
   rl.question('Enter the word to remove: ', (word) => {
-    // TODO: Implement this function
+    removeWordFromFile(filename, word);
     rl.close();
   });
 });
